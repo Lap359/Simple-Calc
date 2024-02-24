cc2df8dc4b9652043135908e32f7730a48e52085
Administrator <Administrator@IDEAPAD-3-15ITL>
2024-02-24 21:31:56 +0700

    Add project files.

---
+++ sources.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int var;
+	cout << "Please choose mode:";
+	cin >> var;
+	if (var = 1); {
+		int x, y;
+		int sum;
+		cout << "Type your number:";
+		cin >> x;
+		cout << "Type another:";
+		cin >> y;
+		sum = x + y;
+		cout << "The result is:" << sum;
+		return 0;
+	}
+	if (var = 2); {
+		int x, y;
+		int sum;
+		cout << "Type your number:";
+		cin >> x;
+		cout << "Type another:";
+		cin >> y;
+		sum = x - y;
+		cout << "The result is:" << sum;
+		return 0;
+	}
+	
+}
